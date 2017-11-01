86e70731aad60a7157477909c1514ad3aea62464
PrebenLeroy <preben.leroy@live.be>
2017-11-01 10:39:17 +0100

    add component team

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
 import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
