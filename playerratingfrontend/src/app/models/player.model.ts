68af3ab1a5daa1ba2a2994fcb4b3ba82f14c6fba
PrebenLeroy <preben.leroy@live.be>
2017-11-16 23:02:48 +0100

    fill the models

--- playerratingfrontend/src/app/models/player.model.ts
+++ playerratingfrontend/src/app/models/player.model.ts
@@ -1,2 +1,42 @@
+import { Position } from './position.model';
+
 export class Player {
+
+    private _firstname: String;
+    private _lastname: String;
+    private _position: Position;
+
+    constructor(firstname: String, lastname: String, position: Position){
+        this._firstname = firstname;
+        this._lastname = lastname;
+        this._position = position;
+    }
+
+    get firstname(){
+        return this._firstname;
+    }
+
+    set firstname(firstname: String){
+        this._firstname = firstname;
+    }
+
+    get lastname(){
+        return this._lastname;
+    }
+
+    set lastname(lastname: String){
+        this._lastname = lastname;
+    }
+
+    get position(){
+        return this._position;
+    }
+
+    set position(position: Position){
+        this._position = position;
+    }
+
+    get positionString(){
+        return Position[this._position];
+    }
 }
--- playerratingfrontend/src/app/models/team.model.ts
+++ playerratingfrontend/src/app/models/team.model.ts
@@ -1,2 +1,28 @@
+import { Player } from './player.model';
+
 export class Team {
+
+    private _name: String;
+    private _players: Player[];
+
+    constructor(name: String, players: Player[]){
+        this._name = name;
+        this._players = players;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name: String){
+        this._name = name;
+    }
+
+    get players(){
+        return this._players;
+    }
+
+    set players(players: Player[]){
+        this._players = players;
+    }
 }
